f4aeaa2cd5b3f7cb6be235bc018b9551604bea18
Amir Hossein Fattahi <amixsty@gmail.com>
2022-06-02 18:19:42 +0430

    new version

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ class Snake(arcade.Sprite):
         self.height = 16
         self.center_x = 250
         self.center_y = 250
+        self.speed = 4
     
     def draw(self):
         arcade.draw_rectangle_filled(self.center_x,self.center_y,self.width,self.height,self.color)
@@ -32,21 +33,21 @@ class Game(arcade.Window):
         super().__init__(width=500,height=400,title="Amixsty Snake :)")
         self.background_color = arcade.color.SAFFRON
         self.food = Apple()
-        self.player = Snake()
+        self.snake = Snake()
 
     def on_key_release(self, symbol: int, modifiers: int):
-        if symbol == arcade.key.W:
-            print("up")
-        elif symbol == arcade.key.S:
-            print("down")    
-        elif symbol == arcade.key.A:
-            print("left")  
-        elif symbol == arcade.key.D:
-            print("right")              
+        if symbol == arcade.key.UP:
+            self.snake.center_y += self.snake.speed
+        elif symbol == arcade.key.DOWN:
+            self.snake.center_y -= self.snake.speed 
+        elif symbol == arcade.key.LEFT:
+            self.snake.center_x -= self.snake.speed 
+        elif symbol == arcade.key.RIGHT:
+            self.snake.center_x += self.snake.speed             
     def on_draw(self):
         arcade.start_render()
         self.food.draw()
-        self.player.draw()
+        self.snake.draw()
 
 
 
